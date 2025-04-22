695cdb7cce3c6c6744ba6048325c31328acafc02
OneState Packages <one.state.developers@gmail.com>
2025-04-22 23:02:02 +0200

    Update door-package-card.js

--- door-package-card.js
+++ door-package-card.js
@@ -975,7 +975,7 @@ class DoorPackageCard extends HTMLElement {
           <div class="contenuto">
             <img
               class="img-porta"
-              src="/local/community/door-package-card/porta-${doorState.toLowerCase()}.png"
+              src="/local/community/ha-card-porta-ingresso/porta-${doorState.toLowerCase()}.png"
               alt="Stato Porta"
             />
             <div class="pulsanti-laterali">
